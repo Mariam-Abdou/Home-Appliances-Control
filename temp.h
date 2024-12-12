0802d1bd9868fa1800d8677b39e239ee741a5350
Hamsaahmad <142181666+Hamsaahmad@users.noreply.github.com>
2024-12-12 17:03:39 +0200

    added buzzer

--- temp.h
+++ temp.h
@@ -7,7 +7,10 @@
 #define LM35_PIN 3  // PE3 for AIN0 (ADC0 channel 0)
 
 /* Function prototypes */
-void ADC_init(void);           // Initialize ADC and GPIO for LM35
-int read_temp(void);           // Read the temperature in Celsius
+void buzzer_init(void);
+void ADC_init(void);
+void buzzer_on(void);
+void buzzer_off(void);
+uint32 read_temp(void);  
 
 #endif /* TEMP_H */
