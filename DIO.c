384a8c5c85ddb7b89b08d81f2a71620c0741e57a
Mariam-Abdou <126914458+Mariam-Abdou@users.noreply.github.com>
2024-12-13 03:52:12 +0200

    edit DIO

--- DIO.c
+++ DIO.c
@@ -1,120 +1,92 @@
-
 #include "DIO.h"
-#include "bitwise_operation.h"
 
-
-
-uint32_t get_port_base(char port) {
+static uint32_t get_port_base(uint8_t port) {
     switch (port) {
-        case 'A': return GPIO_PORTA_BASE;
-        case 'B': return GPIO_PORTB_BASE;
-        case 'C': return GPIO_PORTC_BASE;
-        case 'D': return GPIO_PORTD_BASE;
-        case 'E': return GPIO_PORTE_BASE;
-        case 'F': return GPIO_PORTF_BASE;
-        default: return 0; // Invalid port
+        case 'A':
+		case 'a':
+			return GPIO_PORTA_BASE;
+        case 'B':
+		case 'b':
+			return GPIO_PORTB_BASE;
+        case 'C': 
+		case 'c':
+			return GPIO_PORTC_BASE;
+        case 'D':
+		case 'd':
+			return GPIO_PORTD_BASE;
+        case 'E':
+		case 'e':
+			return GPIO_PORTE_BASE;
+        case 'F':
+		case 'f':
+			return GPIO_PORTF_BASE;
+        default:	// Invalid port
+			return 0;
     }
 }
 
-
-
-void dio_init(char port, uint8_t pin, uint8_t direction, uint8_t pull, uint8_t digital) {
-
+void dio_init(uint8_t port, uint8_t pin, uint8_t direction, uint8_t mode) {
     uint32_t port_base = get_port_base(port);
     if (port_base == 0) return; // Invalid port check
 
-
     // Enable the clock for the port
     SET_BIT(SYSCTL_RCGCGPIO_R, (port - 'A'));   
-
     while ((SYSCTL_PRGPIO_R & (1U << (port - 'A'))) == 0); // Wait for the port to be ready
 
     // Unlock mechanism for locked pins
-    GPIO_PORT_LOCK_R(port_base) = GPIO_LOCK_KEY;  // Unlock the port
-    SET_BIT(GPIO_PORT_CR_R(port_base), pin);   // Commit register for the pin
+    GPIO_PORT_LOCK_R(port_base) = GPIO_LOCK_KEY;	// Unlock the port
+    SET_BIT(GPIO_PORT_CR_R(port_base), pin);		// Commit register for the pin
     
-
-    // Set the direction of the pin (input or output)
-    if (direction == INPUT) {
-        CLEAR_BIT(GPIO_PORT_DIR_R(port_base), pin);  
-
-        // Set the pull-up or pull-down
-        if(pull)
-            SET_BIT(GPIO_PORT_PUR_R(port_base), pin);
-        else
-            CLEAR_BIT(GPIO_PORT_PUR_R(port_base), pin);
-    } 
-    else
-        SET_BIT(GPIO_PORT_DIR_R(port_base), pin);  
-    
- 
-
+    // Set the direction of the pin
+    if (direction)
+        SET_BIT(GPIO_PORT_DIR_R(port_base), pin);
+	else
+		CLEAR_BIT(GPIO_PORT_DIR_R(port_base), pin);  
 
     // Set the mode of the pin (digital or analog)
-    if(digital)
+    if(mode)
         SET_BIT(GPIO_PORT_DEN_R(port_base), pin);
     else
         CLEAR_BIT(GPIO_PORT_DEN_R(port_base), pin);
-
-
-
 }
 
+void dio_pull(uint8_t port, uint8_t pin, uint8_t pull) {
+	uint32_t port_base = get_port_base(port);
+    if (port_base == 0) return;
+	
+	if(pull)
+		SET_BIT(GPIO_PORT_PUR_R(port_base), pin);
+	else
+		CLEAR_BIT(GPIO_PORT_PUR_R(port_base), pin);
+}
 
-uint8_t dio_readpin(char port, uint8_t pin) {
+uint8_t dio_readpin(uint8_t port, uint8_t pin) {
     uint32_t port_base = get_port_base(port);
-    if (port_base == 0) return 0xFF; // Invalid port check //?
+    if (port_base == 0) return 0xFF;
 
     return GET_BIT(GPIO_PORT_DATA_R(port_base), pin);
 }
 
-
-uint8_t dio_readport(char port) {
+uint8_t dio_readport(uint8_t port) {
     uint32_t port_base = get_port_base(port);
-    if (port_base == 0) return 0xFF; // Invalid port check //?
+    if (port_base == 0) return 0xFF;
 
-    // Read the port's value (from pins 0 to 7)
     return (uint8_t) GPIO_PORT_DATA_R(port_base);
 }
 
-
-void dio_writepin(char port, uint8_t pin, uint8_t value) {
+void dio_writepin(uint8_t port, uint8_t pin, uint8_t value) {
     uint32_t port_base = get_port_base(port);
-    if (port_base == 0) return; // Invalid port check
-
-
+    if (port_base == 0) return;
     
-    // if (value) {
-    //     // SET_BIT(*(volatile uint32_t *)(port_base + pin << 2), pin); 
-    //     GPIO_PORT_DATA_BITS_R(port_base)[pin] = 1; //?
-
-    // } else {
-    //     // CLEAR_BIT(*(volatile uint32_t *)(port_base + pin << 2), pin); 
-    //     GPIO_PORT_DATA_BITS_R(port_base)[pin] = 0; //?
-
-    // } // ?
-
-    if(value)
-        SET_BIT(GPIO_PORT_DATA_BITS_R(port_base)[pin], pin);
+	if (value)
+        SET_BIT(GPIO_PORT_DATA_R(port_base), pin);
     else
-        CLEAR_BIT(GPIO_PORT_DATA_BITS_R(port_base)[pin], pin);
-    //value = (value != 0) ? 1 : 0; // Make sure the value is either 0 or 1
-
-    // Write the value to the pin
-    GPIO_PORT_DATA_BITS_R(port_base)[pin] = value;
-
-
+        CLEAR_BIT(GPIO_PORT_DATA_R(port_base), pin);
 }
 
-
-void dio_writeport(char port, uint8_t value) {
+void dio_writeport(uint8_t port, uint8_t value) {
     uint32_t port_base = get_port_base(port);
-    if (port_base == 0) return; // Invalid port check
-
+    if (port_base == 0) return; 
     
-    //value = (value != 0) ? 1 : 0; // Make sure the value is either 0 or 1
-
-    // Write value to the port
     GPIO_PORT_DATA_R(port_base) = value;
-
 }
--- DIO.h
+++ DIO.h
@@ -1,25 +1,24 @@
-
 #ifndef DIO_H
 #define DIO_H
 
 #include <stdint.h>
-#include "TM4C123GH6PM.h"  
+#include "tm4c123gh6pm.h"  
+#include "utils.h"
 
 // Define the direction of a pin
-#define OUTPUT 1
-#define INPUT  0
+#define OUT 1
+#define IN 0
 
 // Define the mode of a pin
 #define DIGITAL 1
 #define ANALOG 0
 
 // Define the pull-up or pull-down of a pin
-#define PULL_UP 1
-#define PULL_DOWN 0
+#define UP 1
+#define DOWN 0
 
 // Define the unlock code
 #define GPIO_LOCK_KEY 0x4C4F434B
-
 
 
 // Define the pins
@@ -41,20 +40,20 @@
 #define GPIO_PORTF_BASE  0x40025000
 
 // Define the necessary registers for each port
-#define GPIO_PORT_DIR_R(port_base)       (*(volatile uint32_t *)(port_base + 0x400))  // Direction register
-#define GPIO_PORT_DEN_R(port_base)       (*(volatile uint32_t *)(port_base + 0x51C))  // Digital enable register
-#define GPIO_PORT_PUR_R(port_base)       (*(volatile uint32_t *)(port_base + 0x510))  // Pull-up resistor register
-#define GPIO_PORT_LOCK_R(port_base)      (*(volatile uint32_t *)(port_base + 0x520))  // Lock register
-#define GPIO_PORT_CR_R(port_base)        (*(volatile uint32_t *)(port_base + 0x524))  // Commit register
-#define GPIO_PORT_DATA_BITS_R(port_base) ((volatile uint32_t *)(port_base))           // Data bits register
-#define GPIO_PORT_DATA_R(port_base)      (*(volatile uint32_t *)(port_base + 0x3FC)) // Data register
+#define GPIO_PORT_DIR_R(port_base)       (*(volatile uint32_t *)(port_base + 0x400))	// Direction register
+#define GPIO_PORT_DEN_R(port_base)       (*(volatile uint32_t *)(port_base + 0x51C))	// Digital enable register
+#define GPIO_PORT_PUR_R(port_base)       (*(volatile uint32_t *)(port_base + 0x510))	// Pull-up resistor register
+#define GPIO_PORT_LOCK_R(port_base)      (*(volatile uint32_t *)(port_base + 0x520))	// Lock register
+#define GPIO_PORT_CR_R(port_base)        (*(volatile uint32_t *)(port_base + 0x524))	// Commit register
+#define GPIO_PORT_DATA_R(port_base)      (*(volatile uint32_t *)(port_base + 0x3FC))	// Data register
+#define GPIO_PORT_DATA_BITS_R(port_base) ((volatile uint32_t *)(port_base))				// Data bits register
 
 // Function prototypes
-void dio_init(char port, uint8_t pins, uint8_t direction, uint8_t pull, uint8_t digital);  
-uint8_t dio_readpin(char port, uint8_t pin);
-uint8_t dio_readport(char port);
-void dio_writepin(char port, uint8_t pin, uint8_t value);
-void dio_writeport(char port, uint8_t value);
-
+void dio_init(uint8_t port, uint8_t pin, uint8_t direction, uint8_t digital);
+void dio_pull(uint8_t port, uint8_t pin, uint8_t pull);
+uint8_t dio_readport(uint8_t port);
+uint8_t dio_readpin(uint8_t port, uint8_t pin);
+void dio_writeport(uint8_t port, uint8_t value);
+void dio_writepin(uint8_t port, uint8_t pin, uint8_t value);
 
 #endif // DIO_H
