7c8d62114f0a1679f548751ec7efc44b57cb9c85
Malak Malak <malaksalem2003@gmail.com>
2024-12-20 14:56:36 +0200

    bluetooth and uart

--- bluetooth_module.h
+++ bluetooth_module.h
@@ -1,41 +1,12 @@
 #ifndef BLUETOOTH_MODULE_H
 #define BLUETOOTH_MODULE_H
+#include "uart.h"
 
-#include <stdbool.h>
+void Bluetooth_Init(void (*callback)(void));
 
-// Bluetooth Module Configuration Enum
-typedef enum {
-    BLUETOOTH_MODE_COMMAND,
-    BLUETOOTH_MODE_DATA,
-    BLUETOOTH_MODE_PAIRING
-} BluetoothMode;
+void Bluetooth_SendData(const uint8_t* data);
+uint8_t Bluetooth_ReceiveData(void);
 
-// Bluetooth Connection Status
-typedef enum {
-    BLUETOOTH_DISCONNECTED,
-    BLUETOOTH_CONNECTED,
-    BLUETOOTH_PAIRING
-} BluetoothConnectionStatus;
-
-// Bluetooth Module Initialization Function
-void Bluetooth_Init(void);
-
-// Set Bluetooth Operation Mode
-bool Bluetooth_SetMode(BluetoothMode mode);
-
-// Send AT Command to Bluetooth Module
-bool Bluetooth_SendCommand(const char* command);
-
-// Send Data via Bluetooth
-bool Bluetooth_SendData(const char* data);
-
-// Check Bluetooth Connection Status
-BluetoothConnectionStatus Bluetooth_GetConnectionStatus(void);
-
-// Start Bluetooth Pairing
-bool Bluetooth_StartPairing(void);
-
-// Stop Bluetooth Pairing
-bool Bluetooth_StopPairing(void);
+void Bluetooth_Handler(void);
 
 #endif // BLUETOOTH_MODULE_H
